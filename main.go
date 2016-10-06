20ccf896527e9e16216f99cb42c4f86437bb1662
pete314 <webpmaster@gmail.com>
2016-10-06 13:47:45 +0100

    4. Add a static HTML page resource to your web application. Have it served...

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 //Author: Peter Nagy - https://github.com/pete314
 //Since: 2016.10.06.
-//Description:
+//Adapted-from: https://go-macaron.com/
+//Description: Main for simple website
 
-package go_simple_site
+package main
+
+import(
+	"gopkg.in/macaron.v1"
+)
+
+func main(){
+	m := macaron.Classic()
+	m.Use(macaron.Renderer())
+
+	m.Get("/", func(ctx *macaron.Context) {
+		//ctx.Data["Name"] = "jeremy"
+		ctx.HTML(200, "index") // 200 is the response code.
+	})
+
+	m.Run()
+}
